9126dd3da1b92ca0a81493dc2ec0ae56486562d9
Timur Turaev <doxxeracm@gmail.com>
2015-03-06 19:49:39 +0300

    Initial Commit

---
+++ TwelveStatements/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TwelveStatements
+//
+//  Created by Тимур on 06.03.15.
+//  Copyright (c) 2015 SPbAU RAS. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
